c62e199c36658c7d991ae51467a748a7e8ed7c19
Abhishek <abhishek.sawant@simformsolutions.com>
2023-04-20 09:49:09 +0530

    DOM Practice

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,47 @@
 'use strict';
+
+let myNumber = Math.floor(Math.random() * 20);
+const theNumber = document.querySelector('.number');
+const again = document.querySelector('.again');
+const inputValue = document.querySelector('.guess');
+const check = document.querySelector('.check');
+const result = document.querySelector('.message');
+const score = document.querySelector('.score');
+const highScore = document.querySelector('.highscore');
+const body = document.querySelector('body');
+
+again.addEventListener('click', () => {
+  body.style.backgroundColor = '#222';
+  myNumber = Math.floor(Math.random() * 20);
+  result.textContent = 'Start guessing...';
+  score.textContent = '20';
+  theNumber.textContent = '?';
+});
+
+function updateScore(score, scoreElem) {
+  score = score - 1;
+  scoreElem.textContent = score;
+}
+
+function updateHighScore(score, scoreElem) {
+  if (score > Number(highScore.textContent)) {
+    scoreElem.textContent = score;
+  }
+}
+
+check.addEventListener('click', () => {
+  if (inputValue.valueAsNumber > myNumber) {
+    result.textContent = 'Too high...';
+    updateScore(Number(score.textContent), score);
+  } else if (inputValue.valueAsNumber < myNumber) {
+    result.textContent = 'Too low...';
+    updateScore(Number(score.textContent), score);
+  } else if (inputValue.valueAsNumber === myNumber) {
+    result.textContent = 'You win...';
+    updateHighScore(Number(score.textContent), highScore);
+    body.style.backgroundColor = '#60b347';
+    theNumber.textContent = score;
+  } else {
+    result.textContent = 'Please enter a valid number...';
+  }
+});
--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,66 @@
 'use strict';
+
+const playerOne = {
+  elem: document.querySelector('.player--0'),
+  score: document.querySelector('#score--0'),
+  curr: document.querySelector('#current--0'),
+};
+
+const playerTwo = {
+  elem: document.querySelector('.player--1'),
+  score: document.querySelector('#score--1'),
+  curr: document.querySelector('#current--1'),
+};
+
+const newGame = document.querySelector('.btn--new');
+const rollDice = document.querySelector('.btn--roll');
+const holdScore = document.querySelector('.btn--hold');
+const dice = document.querySelector('.dice');
+let activePlayer = playerOne;
+let inactivePlayer = playerTwo;
+
+function switchPlayers() {
+  [activePlayer, inactivePlayer] = [inactivePlayer, activePlayer];
+  activePlayer.elem.classList.add('player--active');
+  inactivePlayer.elem.classList.remove('player--active');
+}
+
+function clearCurrent() {
+  activePlayer.curr.textContent = 0;
+}
+
+function updateCurrent(random) {
+  activePlayer.curr.textContent =
+    Number(activePlayer.curr.textContent) + random;
+}
+
+function hold() {
+  activePlayer.score.textContent =
+    Number(activePlayer.score.textContent) +
+    Number(activePlayer.curr.textContent);
+  activePlayer.curr.textContent = 0;
+  dice.src = 'dice-1.png';
+  switchPlayers();
+}
+
+function play() {
+  const random = Math.trunc(Math.random() * 6 + 1);
+  dice.src = `dice-${random}.png`;
+  updateCurrent(random);
+  if (random === 1) {
+    clearCurrent();
+    switchPlayers();
+  }
+  return random;
+}
+
+function clearAll() {
+  playerOne.score.textContent = 0;
+  playerOne.curr.textContent = 0;
+  playerTwo.score.textContent = 0;
+  playerTwo.curr.textContent = 0;
+}
+
+rollDice.addEventListener('click', play);
+holdScore.addEventListener('click', hold);
+newGame.addEventListener('click', clearAll);
